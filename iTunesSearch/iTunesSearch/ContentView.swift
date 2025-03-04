63923cd06c8278d818ac707a1160d6c04d248c4e
smhallman10 <smhallman@unc.edu>
2025-03-03 23:14:40 -0500

    Add swiftformat for autograder

--- iTunesSearch/iTunesSearch/ContentView.swift
+++ iTunesSearch/iTunesSearch/ContentView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-//1. Find API info
-//2. Make URL
-//3. Make request with URLSession
-//4. Create Codable Types to match this JSON
-//5. Use JSONDecoder to fill in Swift types from Data
-//6. Update State
+// 1. Find API info
+// 2. Make URL
+// 3. Make request with URLSession
+// 4. Create Codable Types to match this JSON
+// 5. Use JSONDecoder to fill in Swift types from Data
+// 6. Update State
 
-struct iTunesSearchResponse: Codable{
+struct iTunesSearchResponse: Codable {
     var results: [iTunesSearchResult]
 }
 
@@ -27,36 +27,31 @@ struct iTunesSearchResult: Codable {
 struct ContentView: View {
     @State private var results = [iTunesSearchResult]()
     
-    
     var body: some View {
-        List(results, id: \.trackId){ item in
-            VStack(alignment: .leading){
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
                 Text(item.trackName).font(.headline)
                 Text(item.collectionName)
-        }
-        }.task{
+            }
+        }.task {
             await loadData()
         }
-     
     }
     
-    func loadData() async{
-        guard let url = URL(string: "https://itunes.apple.com/search?term=beatless&entity=song") else{
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=beatless&entity=song") else {
             print("Invalid URL")
             return
         }
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(iTunesSearchResponse.self, from: data){
+            if let decodedResponse = try? JSONDecoder().decode(iTunesSearchResponse.self, from: data) {
                 results = decodedResponse.results
             }
         } catch {
             print("Invalid Data")
         }
-        
     }
-    
-    
 }
 
 #Preview {
